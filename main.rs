5842d2bd912088d7f3acd5e1101d3ed37195c33c
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-10-28 21:47:27 -0300

    Entrada e saída de dados

--- main.rs
+++ main.rs
@@ -4,6 +4,7 @@ use std::io::prelude::*;
 fn main() {
     println!("Programaçao na linguagem rust\n");
     print!("Tecle <Enter> para encerrar...");
+    
     io::stdout().flush().unwrap();
     io::stdin().read(&mut [0u8]).unwrap();
 }
