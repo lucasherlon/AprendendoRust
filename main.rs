a3df3966d1199d77b4eccc3bec47facf2d73c893
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-10-29 20:04:15 -0300

    entrada e saída de dados ex02

--- main.rs
+++ main.rs
@@ -2,9 +2,23 @@ use std::io;
 use std::io::prelude::*;
 
 fn main() {
-    println!("Programaçao na linguagem rust\n");
+    let mut nome = String::new();
+
+    print!("Informe seu nome: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut nome).expect("Entrada incorreta");
+
+    if let Some('\n') = nome.chars().next_back() {
+        nome.pop();
+    }
+
+    if let Some('\r') = nome.chars().next_back() {
+        nome.pop();
+    }
+
+    println!("Olá, {}\n", nome);
     print!("Tecle <Enter> para encerrar...");
-    
     io::stdout().flush().unwrap();
     io::stdin().read(&mut [0u8]).unwrap();
+    
 }
