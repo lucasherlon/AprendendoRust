c90982de950f210c2cf6bd76f93669ed57567147
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-11-04 20:23:47 -0300

    bases numericas

---
+++ c02ex09.rs
@@ -0,0 +1,19 @@
+use std::io;
+use std::io::prelude::*;
+
+fn main() {
+    let valor = 10;
+
+    println!("Binário: {:b}", valor);
+    println!("Octal: {:o}", valor);
+    println!("Hexadecimal: {:x}", valor);
+
+    println!();
+    println!("ou");
+    println!("BIN: {:b} OCT: {:o} HEX: {:x}", valor, valor, valor);
+
+    println!();
+    print!("Tecle <Enter> para encerrar...");
+    io::stdout().flush().unwrap();
+    io::stdin().read(&mut [0u8]).unwrap();
+}
