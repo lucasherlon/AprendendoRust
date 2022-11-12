c0fbbdb7c81fc50f3bfe08cc786576beb5305333
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-11-12 18:43:52 -0300

    ex de fixaçao 03 do cap 02

---
+++ c02fix03.rs
@@ -0,0 +1,25 @@
+use std::io;
+use std::io::prelude::*;
+
+fn main() {
+    let mut _a = String::new();
+    let mut _b = String::new();
+    let aux: i32;
+    
+    print!("Digite o número A: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut _a).expect("Erro de input: linha 11.");
+    let mut a: i32 = _a.trim().parse::<i32>().expect("Erro de conversão: linha 12.");
+
+    print!("Digite o número B: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut _b).expect("Erro de input: linha 10.");
+    let mut b: i32 = _b.trim().parse::<i32>().expect("Erro de conversão: linha 12.");
+
+    aux = a;
+    a = b;
+    b = aux;
+
+    println!("Novo valor de A: {}", a);
+    println!("Novo valor de B: {}", b);
+}
