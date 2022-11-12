de2e56811b1bd7f3253ce28ae85eeaa4eb6dbf2b
Lucas Herlon <62165683+lucasherlon@users.noreply.github.com>
2022-11-12 18:02:16 -0300

    ex de fixaçao 01 do cap 02

--- c02fix01.rs
+++ c02fix01.rs
@@ -8,7 +8,7 @@ fn main() {
     io::stdin().read_line(&mut celsius).unwrap();
 
     let c: f64 = celsius.trim().parse::<f64>().unwrap();
-    let f: f64 = (9.*c + 160.)/9.;
+    let f: f64 = (9.*c + 160.)/5.;
 
     println!("{} grau(s) celsius equivale(m) a {:.2} grau(s) fahrenheiht", c, f);
 
